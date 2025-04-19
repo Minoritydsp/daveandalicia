13ec385cc7597424490b2ec0ae4366f8491875da
davesebastian17@gmail.com <davesebastian17@gmail.com>
2025-04-19 22:57:33 +0700

    j

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-};
-
-module.exports = {
   images: {
     unoptimized: true,
   },
 };
+
+module.exports = nextConfig;
